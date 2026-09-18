6b836327244cec41aed6618bcc842e7580b6abb0
labukhan153-beep <labukhan153@gmail.com>
2026-09-18 12:24:01 +0600

    Delete contact directory

--- contact/page.js
+++
@@ -1,8 +0,0 @@
-export default function ContactPage() {
-  return (
-    <div style={{ padding: '20px', color: '#fff', backgroundColor: '#000', minHeight: '100vh' }}>
-      <h1>Contact Us</h1>
-      <p>Welcome to the Contact page of Convalt Energy.</p>
-    </div>
-  );
-}
